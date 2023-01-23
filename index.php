b344580b406959713ffd0dd5bbef7c9634feadce
Andrey <Boun222333@gmail.com>
2023-01-23 22:43:52 +0300

    Start window

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;400;700&family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Football fields</title>
+</head>
+<body class="body_index">
+     <header class="header">
+        <div class="container-fluid">
+             <div class="row">
+                <div class="col">
+                    <div class="nav-item nav-b"><b>Football fields</b></div>
+                 </div>
+            </div>
+        </div>
+    </header>
+     <main class="main_index">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <div class="index-item big">
+                    «Football fields» был разработан дял поиска футбольных полей и площадок в Мооскве. Пользователь может найти 
+                    ближайшую площадку от его местоположения, а так же при помощи фильтров выбрать самую удобную площадку.
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <a href="map.php"><button class="button">Продолжить</button></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <div class="index-item">Источники на используемые открытые данные:</div>
+                    <div class="index-item"><a href="https://data.mos.ru/opendata/893" target="_blank">Спортивные площадки</a></div>
+                    <div class="index-item"><a href="https://data.mos.ru/opendata/886" target="_blank">Поля футбольные</a></div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="copyright"><b>© Football fields, 2023</b></div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
